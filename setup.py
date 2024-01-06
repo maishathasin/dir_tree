d7d743e44ecd081c981de4f5bb8af825f90e57b9
Maisha Thasin <thasin.maisha@gmail.com>
2024-01-05 22:23:10 -0500

    d

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dir_tree',
+    name='repo_tree',
     version='0.1',
     packages=find_packages(),
     description='Utility Package that visualises Tree Structure of Github repos using Personal tokens and basic dictionaries',
